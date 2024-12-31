7b79254df669a5d78ab1eb31ed5fa949587d9643
Mathieu-van-Luijken <M.f.a.c.v.luijken@student.tue.nl>
2024-12-31 14:24:50 +0100

    First lines of PPO agent

---
+++ kits/python/ppo_agent.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn.functional as F
+from ...src.luxai_s3.env import LuxAIS3Env
+
+class PPOAgent:
+    def __init__():
+        # self.device = torch.device(device_id)
+        env = LuxAIS3Env()
